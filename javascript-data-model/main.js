227d0f5321a1508c99daca7849b8bddfb9086f85
Phillip Lam <plam89@ucla.edu>
2020-10-13 10:51:52 -0700

    Fixed data types, they are the same now

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,20 +2,20 @@
 
 var orderHistory = [
   {
-    itemNames: 'JavaScript for Impatient Programmers',
-    type: 'book',
+    itemNames: ['JavaScript for Impatient Programmers'],
+    type: ['book'],
     returnDeadline: 'September 7, 2020',
     orderNumber: '114-394'
   },
   {
-    itemNames: 'The Timless Way of Building',
-    type: 'book',
+    itemNames: ['The Timless Way of Building'],
+    type: ['book'],
     returnDeadline: 'August 19, 2020',
     orderNumber: '113-998'
   },
   {
-    itemNames: 'Gamecube Controller Adapter',
-    type: 'game acessories',
+    itemNames: ['Gamecube Controller Adapter'],
+    type: ['game acessories'],
     returnDeadline: 'August 5, 2020',
     orderNumber: '114-287'
   },
