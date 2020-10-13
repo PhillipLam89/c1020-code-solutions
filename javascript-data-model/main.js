d68ee8f80de8f55c4462e14fb18e711bdc084ffb
Phillip Lam <plam89@ucla.edu>
2020-10-13 16:28:41 -0700

    fixed to include array of objects of items

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -3,61 +3,68 @@
 var orderHistory = [
   {
     orderInfo: {
-      booksOrdered: {
-        itemName: 'JavaScript for Impatient Programmers',
-        itemType: 'book',
-        itemCost: 31.55,
-        itemReturnDeadline: 'September 7, 2020',
-        orderNumber: '114-394',
-        orderTotalAfterTaxes: 34.00
-      },
-      gamesOrdered: null,
+      ShipTo: 'JS Masher',
+      orderDate: 'August 4, 2020',
       orderNumber: '114-394',
-      orderTotalAfterTaxes: 34.00
-    }
+      orderTotalAfterTaxes: 34.00,
+      deliveryDate: 'July 8, 2020'
+    },
+    items: [{
+      category: 'books',
+      title: 'Javascript for Impatient Programmers',
+      author: 'Dr.AXel Rauschmayer',
+      itemPrice: 31.55
+    }]
   },
+
   {
     orderInfo: {
-      booksOrdered: {
-        itemName: 'The Timeless Way of building',
-        itemType: 'book',
-        itemCost: 41.53,
-        itemReturnDeadline: 'September 7, 2020'
-      },
-      gamesOrdered: null,
+      ShipTo: 'JS Masher',
+      orderDate: 'July 19, 2020',
       orderNumber: '113-998',
-      orderTotalAfterTaxes: 44.53
-    }
+      orderTotalAfterTaxes: 44.53,
+      deliveryDate: 'July 20, 2020'
+    },
+    items: [{
+      category: 'books',
+      title: 'The Timeless Way of Building',
+      author: 'Dr.AXel Rauschmayer',
+      itemPrice: 41.33
+    }]
   },
+
   {
     orderInfo: {
-      gameOrders: {
-        itemName: 'Gamecube Controller Adapter',
-        itemType: 'accessories',
-        itemCost: 41.53,
-        itemReturnDeadline: 'September 7, 2020'
-      },
-      booksOrdered: null,
+      ShipTo: 'JS Masher',
+      orderDate: 'July 4, 2020',
       orderNumber: '114-287',
-      orderTotalAfterTaxes: 44.53
-    }
+      orderTotalAfterTaxes: 17.22,
+      deliveryDate: 'July 7, 2020'
+    },
+    items: [{
+      category: 'game acessories',
+      title: 'Gamecube Controller Adapter',
+      itemPrice: 31.55
+    }]
   },
+
   {
     orderInfo: {
-      gameOrders: {
-        itemName: 'Game Cube Switch Controller',
-        itemType: 'accessories',
-        itemCost: 94.95,
-        itemReturnDeadline: 'August 4, 2020'
-      },
-      booksOrdered: {
-        itemName: 'The art of SQL',
-        itemType: 'book',
-        itemCost: 33.99,
-        itemReturnDeadline: 'August 4, 2020'
-      },
-      orderNumber: '113-288',
-      orderTotalAfterTaxes: 138.93
-    }
+      ShipTo: 'JS Masher',
+      orderDate: 'July 3, 2020',
+      orderNumber: '115-288',
+      orderTotalAfterTaxes: 138.93,
+      deliveryDate: 'July 5, 2020'
+    },
+    items: [{
+      category: 'books',
+      title: 'The art of SQL',
+      author: 'Faroult, Stephane',
+      itemPrice: 94.5
+    }, {
+      category: 'game acessories',
+      title: 'Gamecube Controller',
+      itemPrice: 94.95
+    }]
   }
 ];
