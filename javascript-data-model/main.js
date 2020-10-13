3ea90929017211afce75ebfae82fc801d8941ee0
Phillip Lam <plam89@ucla.edu>
2020-10-13 10:44:27 -0700

    JS Data Model

---
+++ javascript-data-model/main.js
@@ -0,0 +1,28 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    itemNames: 'JavaScript for Impatient Programmers',
+    type: 'book',
+    returnDeadline: 'September 7, 2020',
+    orderNumber: '114-394'
+  },
+  {
+    itemNames: 'The Timless Way of Building',
+    type: 'book',
+    returnDeadline: 'August 19, 2020',
+    orderNumber: '113-998'
+  },
+  {
+    itemNames: 'Gamecube Controller Adapter',
+    type: 'game acessories',
+    returnDeadline: 'August 5, 2020',
+    orderNumber: '114-287'
+  },
+  {
+    itemNames: ['Gamecube Controler for Smash', 'The art of SQL'],
+    type: ['game acessories', 'book'],
+    returnDeadline: ['August 4, 2020', 'August 4, 2020'],
+    orderNumber: '113-288'
+  }
+];
