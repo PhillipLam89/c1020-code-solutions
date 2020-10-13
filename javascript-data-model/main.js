6cf207f498c2e483bbf54238ea482ec189d54ff7
Phillip Lam <plam89@ucla.edu>
2020-10-13 14:43:42 -0700

    organized structure better

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,31 +2,62 @@
 
 var orderHistory = [
   {
-    itemNames: ['JavaScript for Impatient Programmers'],
-    type: ['book'],
-    returnDeadline: 'September 7, 2020',
-    orderNumber: '114-394',
-    orderTotal: 34.00
+    orderInfo: {
+      booksOrdered: {
+        itemName: 'JavaScript for Impatient Programmers',
+        itemType: 'book',
+        itemCost: 31.55,
+        itemReturnDeadline: 'September 7, 2020',
+        orderNumber: '114-394',
+        orderTotalAfterTaxes: 34.00
+      },
+      gamesOrdered: null,
+      orderNumber: '114-394',
+      orderTotalAfterTaxes: 34.00
+    }
   },
   {
-    itemNames: ['The Timeless Way of Building'],
-    type: ['book'],
-    returnDeadline: 'August 19, 2020',
-    orderNumber: '113-998',
-    orderTotal: 44.53
+    orderInfo: {
+      booksOrdered: {
+        itemName: 'The Timeless Way of building',
+        itemType: 'book',
+        itemCost: 41.53,
+        itemReturnDeadline: 'September 7, 2020'
+      },
+      gamesOrdered: null,
+      orderNumber: '113-998',
+      orderTotalAfterTaxes: 44.53
+    }
   },
   {
-    itemNames: ['Gamecube Controller Adapter'],
-    type: ['game acessories'],
-    returnDeadline: 'August 5, 2020',
-    orderNumber: '114-287',
-    orderTotal: 17.22
+    orderInfo: {
+      gameOrders: {
+        itemName: 'Gamecube Controller Adapter',
+        itemType: 'accessories',
+        itemCost: 41.53,
+        itemReturnDeadline: 'September 7, 2020'
+      },
+      booksOrdered: null,
+      orderNumber: '114-287',
+      orderTotalAfterTaxes: 44.53
+    }
   },
   {
-    itemNames: ['Gamecube Controller for Smash', 'The art of SQL'],
-    type: ['game acessories', 'book'],
-    returnDeadline: ['August 4, 2020', 'August 4, 2020'],
-    orderNumber: '113-288',
-    orderTotal: 138.93
+    orderInfo: {
+      gameOrders: {
+        itemName: 'Game Cube Switch Controller',
+        itemType: 'accessories',
+        itemCost: 94.95,
+        itemReturnDeadline: 'August 4, 2020'
+      },
+      booksOrdered: {
+        itemName: 'The art of SQL',
+        itemType: 'book',
+        itemCost: 33.99,
+        itemReturnDeadline: 'August 4, 2020'
+      },
+      orderNumber: '113-288',
+      orderTotalAfterTaxes: 138.93
+    }
   }
 ];
