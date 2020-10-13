f77415c9ff72a09f97f402cebefea772171b747e
Phillip Lam <plam89@ucla.edu>
2020-10-13 10:59:23 -0700

    Fixed data types, they are the same now, added order total

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -5,24 +5,28 @@ var orderHistory = [
     itemNames: ['JavaScript for Impatient Programmers'],
     type: ['book'],
     returnDeadline: 'September 7, 2020',
-    orderNumber: '114-394'
+    orderNumber: '114-394',
+    orderTotal: 34.00
   },
   {
     itemNames: ['The Timeless Way of Building'],
     type: ['book'],
     returnDeadline: 'August 19, 2020',
-    orderNumber: '113-998'
+    orderNumber: '113-998',
+    orderTotal: 44.53
   },
   {
     itemNames: ['Gamecube Controller Adapter'],
     type: ['game acessories'],
     returnDeadline: 'August 5, 2020',
-    orderNumber: '114-287'
+    orderNumber: '114-287',
+    orderTotal: 17.22
   },
   {
     itemNames: ['Gamecube Controller for Smash', 'The art of SQL'],
     type: ['game acessories', 'book'],
     returnDeadline: ['August 4, 2020', 'August 4, 2020'],
-    orderNumber: '113-288'
+    orderNumber: '113-288',
+    orderTotal: 138.93
   }
 ];
