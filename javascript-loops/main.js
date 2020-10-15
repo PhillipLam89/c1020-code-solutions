29faefc35f6df672c8708162676bfb77d161654a
Phillip Lam <plam89@ucla.edu>
2020-10-15 13:01:08 -0700

    fixed aforementioned errors!

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -8,46 +8,42 @@
  */
 
 function getNumbersToTen() {
-  var i = 1;
   var numbers = [];
   var currentNumber = 1;
-  while (i < 11) {
+  while (currentNumber < 11) {
     numbers.push(currentNumber);
     currentNumber += 1;
-    i++;
   }
   return numbers;
 }
 console.log('numbers 1 to 10', getNumbersToTen());
 
 function getEvenNumbersToTwenty() {
-  var i = 2;
-  var numbers = [];
+  var evenNumbers = [];
   var currentNumber = 2;
-  while (i < 21) {
-    numbers.push(currentNumber);
+  while (currentNumber < 21) {
+    evenNumbers.push(currentNumber);
     currentNumber += 2;
-    i = i + 2;
   }
-  return numbers;
+  return evenNumbers;
 }
 console.log('Even numbers 2 to 20', getEvenNumbersToTwenty());
 
 function repeatWord(word, times) {
   var count = 1;
   var repeated = '';
-  while (count < times + 1) {
+  while (count <= times) {
     repeated += ' ' + word;
     count += 1;
   }
   return repeated;
 }
-console.log('Repeat function:', repeatWord('JAVASCRIPTTT', 22));
+console.log('Repeat function:', repeatWord('JAVASCRIPTTT', 7));
 
 function logEachCharacter(string) {
   var final = '';
   for (var i = 0; i < string.length; i++) {
-    (final = string[i]);
+    final = string[i];
     console.log('Log each char of string:', final);
   }
 }
@@ -56,13 +52,13 @@ logEachCharacter('hDSFN');
 function doubleAll(numbers) {
   var doubled = [];
   for (var i = 0; i < numbers.length; i++) {
-    numbers[i] = numbers[i] * 2;
-    doubled.push(numbers[i]);
+    doubled.push(numbers[i] * 2); // this will NOT mutate orignal array :)
   }
   return doubled;
 }
 
 var num1 = [2, 4, 6, 7];
+console.log('original array:', num1);
 console.log('doubled array:', doubleAll(num1));
 
 var object = { aa: 113, zz: 'boo', bb: '33' };
