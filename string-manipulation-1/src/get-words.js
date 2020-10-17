ba54ec870f1609a4ac59b165346dad5ffb9347c3
Phillip Lam <plam89@ucla.edu>
2020-10-17 00:45:39 -0700

    Finished string manipulation 1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
 
 function getWords(string) {
-
+  var arr = [];
+  if (string.length === 0) {
+    return arr;
+  }
+  arr = string.split(' ');
+  return arr;
 }
--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function reverseWord(word) {
+  var reversedString = '';
+  var lastIndex = word.length - 1;
+  for (var i = lastIndex; i > -1; i--) { // a backwards for loop and concatenation will automatically give a reversed string
+    reversedString += word[i]; // i > -1 will make sure sure loop breaks after first index of zero
+  }
+  return reversedString;
+}
+
+// I cant believe it took me almost 2 hours to get this....
