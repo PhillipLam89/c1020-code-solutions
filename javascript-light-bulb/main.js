060b48162d30131c7faf0c1865afc7562a0e22b8
Phillip Lam <plam89@ucla.edu>
2020-10-20 13:21:15 -0700

    stored info in variables instead of DOM

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -2,13 +2,17 @@
 var selectButton = document.querySelector('.lights-on');
 var container = document.querySelector('.container');
 
-selectButton.addEventListener('click', function () {
+var on = true;
 
-  if (selectButton.className === 'lights-on') {
+selectButton.addEventListener('click', function () {
+  if (on) {
     selectButton.className = 'lights-off';
     container.className = 'container-off';
-  } else if (selectButton.className === 'lights-off') {
+    on = false;
+
+  } else {
     selectButton.className = 'lights-on';
     container.className = 'container';
+    on = true;
   }
 });
