ca0093ebeb2c11e46b5040db525da6a8b3843770
Phillip Lam <plam89@ucla.edu>
2020-10-20 16:47:50 -0700

    Finished Str Manipulation 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,4 +1,12 @@
 /* eslint-disable no-unused-vars */
 function swapChars(firstIndex, secondIndex, string) {
+  var arr = string.split('');
+  var firstPlaceholder = string[firstIndex];
+
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = firstPlaceholder;
+
+  var finalString = arr.join('');
+  return finalString;
 
 }
