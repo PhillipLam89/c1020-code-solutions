8e3b3a30e5e7698f4b8244e21319f546a3c6f07b
Phillip Lam <plam89@ucla.edu>
2020-10-21 12:21:22 -0700

    finishing up

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,18 +1,24 @@
 var $phrase = document.querySelectorAll('span');
 var counter = 0;
 var wrongCount = 0;
+var underlinePosition = 1;
 var $result = document.querySelector('.result');
 var $wrong = document.querySelector('.wrong-count');
+var $percent = document.querySelector('.percentage');
 
 document.querySelector('html').addEventListener('keydown', function (e) {
-  if (e.key === $phrase[counter].textContent) {
+  if (e.key === $phrase[counter].textContent && counter < 30 && underlinePosition <= 29) {
     $phrase[counter].className = 'green';
-    $phrase[counter + 1].className = 'has-underline';
+    $phrase[underlinePosition].className = 'has-underline';
     counter++;
+    underlinePosition++;
     $result.textContent = 'Letters Typed Correctly: ' + counter;
   } else if (e.key !== $phrase[counter].textContent) {
     $phrase[counter].className = 'red';
     wrongCount++;
     $wrong.textContent = 'Letters Typed Wrong: ' + wrongCount;
+  } else if (counter === 29) {
+    $phrase[29].className = 'green';
+    $percent.textContent = 'Your accuracy is: ' + ((counter) / (counter + wrongCount)).toFixed(2) * 100 + '%';
   }
 });
