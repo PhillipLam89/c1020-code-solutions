3cba6fcb49fde8e27aa5fae6af407633c9a04849
Phillip Lam <plam89@ucla.edu>
2020-10-25 01:31:21 -0700

    on reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,18 +1,29 @@
 /* eslint-disable no-unused-vars */
-function reverseWords(string) {
-  var final = '';
-  var a = [...string];
+// function reverseWords(string) {
+//   function reverseString(string) {
+//   var final = ''
+//   var lastIndex = string.length - 1
+//   for (var i = lastIndex; i > -1; i--) {
+//     final += string[i]
+//   }
+//   console.log(final)
+//   return final
+//   }
 
-  var b = [];
+//  var arr = Array.from(string)
+//  console.log('arr:', arr)
 
-  for (var i = 0; i < a.length; i++) {
-    b.push([...a[i]]);
-
+// }
+function reverseWords(input) {
+  var word = ''; var output = '';
+  var lastIndex = input.length - 1;
+  for (var i = lastIndex; i > -1; i--) {
+    if (input[i] !== ' ') {
+      word += input[i];
+    } else {
+      output = ' ' + word + output;
+      word = '';
+    }
   }
-
-  for (var j = 0; j < b.length; j++) {
-    final += b[j];
-  }
-
+  return word + output;
 }
-var a = [['w'], ['h'], ['a'], ['t'], [' '], ['i'], ['s']];
