87631d50e3a2644e0bcedb1dd93997d85aaf62c6
Phillip Lam <plam89@ucla.edu>
2020-11-07 17:01:52 -0800

    finally finished Str Manipulation3. refactored code

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
 
-// function reverseWords(string) {
-//   var reversedSection = '';
-//   var output = '';
-//   var lastIndex = string.length - 1;
-//   for (var i = lastIndex; i > -1; i--) {
-//     if (string[i] !== ' ') {
-//       reversedSection += string[i];
-//     } else {
-//       output = ' ' + reversedSection + output;   // as we iterate backwards from the string, each complete word will be put in reversed form, when it hits a space,  the complete word after that will be placed IN FRONT of the previous word with a space.
-//       reversedSection = '';
-//     }
-//   }
-//   var finalString = reversedSection + output
-//   return finalString
-// }
+function reverseWords(string) {
+  var reversedSection = '';
+  var output = '';
+  var lastIndex = string.length - 1;
+  for (var i = lastIndex; i > -1; i--) {
+    if (string[i] !== ' ') {
+      reversedSection += string[i];
+    } else {
+      output = ' ' + reversedSection + output; // as we iterate backwards from the string, each complete word will be put in reversed form, when it hits a space,  the complete word after that will be placed IN FRONT of the previous word with a space.
+      reversedSection = '';
+    }
+  }
+  var finalString = reversedSection + output;
+  return finalString;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,51 @@
 /* eslint-disable no-unused-vars */
+function titleCase(title) {
+
+  var finalStr = '';
+  var arr = title.split(' ');
+
+  var specialWordsArr = ['a', 'an', 'and', 'at', 'or', 'by', 'to', 'the', 'as', 'per', 'nor', 'for', 'in', 'of', 'on', 'but'];
+
+  for (var i = 0; i < arr.length; i++) {
+    var word = arr[i];
+    if (!(i === 0 || arr[i - 1].includes(':'))) {
+      if (specialWordsArr.includes(word)) {
+        finalStr += ' ' + word;
+        continue;
+      }
+    }
+    if (i === 0) {
+      if (word.toUpperCase() === 'JAVASCRIPT:') {
+        finalStr += 'JavaScript:';
+        continue; // jumps over 1 iteration of loop
+      } else if (word.toUpperCase() === 'API') {
+        finalStr += 'API';
+        continue;
+      }
+    } else {
+      if (word.toUpperCase() === 'JAVASCRIPT:') {
+        finalStr += ' ' + 'JavaScript:';
+        continue;
+      } else if (word.toUpperCase() === 'JAVASCRIPT') {
+        finalStr += ' ' + 'JavaScript';
+        continue;
+      } else if (word.toUpperCase() === 'API') {
+        finalStr += ' API';
+        continue;
+      }
+    }
+    if (word.includes('-')) { // slice and + will concatenate the string and capitalize any letters after -
+      var dashLocation = word.indexOf('-'); // gets location of -
+      word = word.slice(0, dashLocation + 1) + word.slice(dashLocation + 1, dashLocation + 2).toUpperCase() + word.slice(dashLocation + 2);
+    }
+
+    var capital = word.slice(0, 1).toUpperCase() + word.slice(1);
+
+    if (i !== 0) {
+      finalStr += ' ' + capital;
+    } else {
+      finalStr += capital;
+    }
+  }
+  return finalStr;
+}
