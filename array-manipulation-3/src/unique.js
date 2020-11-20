b1c26ffbb7bc2a5cbdb946bf5eb7d9124aa78ee8
Phillip Lam <plam89@ucla.edu>
2020-11-20 01:03:16 -0800

    on difference array problem

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+
+function unique(array) {
+  var finalArr = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!finalArr.includes(array[i])) { // this pushes the values in the final array if it DOESNT currently have that value already
+      finalArr.push(array[i]);
+    }
+  }
+  return finalArr;
+}
