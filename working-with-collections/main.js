a8e190ebc4c1f45afefa516ee010cc5c26ac6ef9
Phillip Lam <plam89@ucla.edu>
2020-11-20 16:31:51 -0800

    working on collections

---
+++ working-with-collections/main.js
@@ -0,0 +1,217 @@
+/* global _ */
+/* eslint-disable no-console */
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+
+// var deck = [
+//   {
+//     rank: 'A',
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 'K',
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 'Q',
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 'J',
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 10,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 9,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 8,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 7,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 6,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 5,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 4,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 3,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 2,
+//     suit: 'Clubs'
+//   },
+//   {
+//     rank: 'A',
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 'K',
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 'Q',
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 'J',
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 10,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 9,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 8,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 7,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 6,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 5,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 4,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 3,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 2,
+//     suit: 'Spades'
+//   },
+//   {
+//     rank: 'A',
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 'K',
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 'Q',
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 'J',
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 10,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 9,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 8,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 7,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 6,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 5,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 4,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 3,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 2,
+//     suit: 'Hearts'
+//   },
+//   {
+//     rank: 'A',
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 'K',
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 'Q',
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 'J',
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 10,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 9,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 8,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 7,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 6,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 5,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 4,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 3,
+//     suit: 'Diamonds'
+//   },
+//   {
+//     rank: 2,
+//     suit: 'Diamonds'
+//   }
+// ];
+
+// var players = [{ name: 'Phillip', hand: [] }, { name: 'Cody', hand: [] }, { name: 'Uzair', hand: [] }, { name: 'Brett', hand: [] }]
