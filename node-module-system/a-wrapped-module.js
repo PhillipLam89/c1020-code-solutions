b9f1ed2be9d8b1f3114cec46c3d72653dd2ced80
Phillip Lam <plam89@ucla.edu>
2020-11-25 10:39:11 -0800

    finished node-module-system assignment

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+
+console.log('type of exports:', typeof exports);
+console.log('value of exports: ', exports);
+console.log('type of require:', typeof require);
+console.log('value of require: ', require);
+console.log('type of module:', typeof module);
+console.log('value of module: ', module);
+console.log('type of __filename:', typeof __filename);
+console.log('value of __filename: ', __filename);
