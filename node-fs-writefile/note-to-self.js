decdb4069f02a924899daa51ea8dc5c34131f871
Phillip Lam <plam89@ucla.edu>
2020-11-30 11:40:32 -0800

    included err argument

--- node-fs-writefile/note-to-self.js
+++ node-fs-writefile/note-to-self.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const fs = require('fs');
-fs.writeFile('note.txt', process.argv[2], 'utf8', function () {
+fs.writeFile('note.txt', process.argv[2], 'utf8', function (err) {
+  if (err) throw err;
   console.log(`note.txt file created with content: ${process.argv[2]}.`);
 });
