b63049a62417430b0ad1f40d41ec51bc0d86e9cb
Phillip Lam <plam89@ucla.edu>
2020-11-29 18:57:55 -0800

    minor fixes

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,7 +1,7 @@
 /* global _ */
 /* eslint-disable no-console */
 
-const currentPlayers = [{ name: 'Phillip', hand: [] }, { name: 'Cody', hand: [] }, { name: 'Uzair', hand: [] }, { name: 'Brett', hand: [] }, { name: 'UzairAA', hand: [] }, { name: 'BrettAA', hand: [] }, { name: 'BrettCCC', hand: [] }, { name: 'BrettAC', hand: [] }, { name: 'Bill', hand: [] }];
+const currentPlayers = [{ name: 'Phillip', hand: [] }, { name: 'Cody', hand: [] }, { name: 'Uzair', hand: [] }, { name: 'Brett', hand: [] }, { name: 'Tim D', hand: [] }];
 let deck = [];
 const cardRanks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const cardSuits = ['clubs','diamonds','hearts','spades']
@@ -68,12 +68,12 @@ function startGame(arrayOfPlayers, cardsPerHand) {
     arr.push(arrayOfPlayers[i].total)
   }
 
-  const IndexOfMaxScore = arr.indexOf(Math.max(...arr))
-  const maxScore = Math.max(...arr)
+  const indexOfMaxScore = arr.indexOf(Math.max(...arr))
   const arrayOfMaxScores = indexesOfMaxScores(arr)  //returns arrays of indexes of tied high scores, these indixes will identify players with tied high scores  in the arrayOfPlayers array passed in.
 
   if (arrayOfMaxScores.length > 1) {
-    let tieMessage = ''
+    let tieMessage = `WHOA THERES ${arrayOfMaxScores.length} PLAYERS TIED FOR FIRST PLACE:
+    `
     const tiedPlayers = []
     for (let i = 0; i < arrayOfMaxScores.length; i++ ) {
       tiedPlayers.push(arrayOfPlayers[arrayOfMaxScores[i]]) // [arrayOfMaxScores[i]] allows us to identify the index of the tied players
@@ -86,7 +86,7 @@ function startGame(arrayOfPlayers, cardsPerHand) {
       }
     }
     tieMessage += `
-    These hands are tied with ${arr[IndexOfMaxScore]} points each. A tie breaker will now run...`
+    These hands are tied with ${arr[indexOfMaxScore]} points each. A tie breaker will now run...`
     console.log(tieMessage)
     console.log(`RESULT OF TIE BREAKER: `)
     startGame(tiedPlayers, cardsPerHand)
@@ -94,18 +94,26 @@ function startGame(arrayOfPlayers, cardsPerHand) {
   }
   let winningHand = ''
 
-  console.log(`The winner is ${arrayOfPlayers[IndexOfMaxScore].name.toUpperCase()}! With a total of ${arrayOfPlayers[IndexOfMaxScore].total} points. `)
-  for (let val of arrayOfPlayers[IndexOfMaxScore].hand ) { //displays winning hand
+  console.log(`The winner is ${arrayOfPlayers[indexOfMaxScore].name.toUpperCase()}! With a total of ${arrayOfPlayers[indexOfMaxScore].total} points. `)
+  for (let val of arrayOfPlayers[indexOfMaxScore].hand ) { //displays winning hand
     winningHand += ' |' + val.rank + ' of ' + val.suit + '|'
   }
   console.log(`Winning hand: ${winningHand}.`);
   winningHand = ''
   const participantsList = []
+  let displayAllHands = ''
   for (let i = 0; i < arrayOfPlayers.length;i++) {
     participantsList.push(arrayOfPlayers[i].name)
-    console.log('Hand Dealt:', arrayOfPlayers[i].name , arrayOfPlayers[i].hand)
+    displayAllHands += `
+    ${arrayOfPlayers[i].name}'s hand:
+    `
+    for (let j = 0; j < arrayOfPlayers[i].hand.length; j++) {
+      displayAllHands += ` |${arrayOfPlayers[i].hand[j].rank} of ${arrayOfPlayers[i].hand[j].suit}|
+    `
+    }
 
   }
+  console.log(displayAllHands)
   console.log(`Final Round Participants: ${participantsList.join(', ')}`)
   console.log(`All players' hands have been resetted`)
   console.log(`Create a new player by typing addNewPlayer(playerNameHere), or delete a player with removePlayer(playerNameHere) then/or start a new game by typing startGame(currentPlayers, numbersOfCardsEachHand)`)
