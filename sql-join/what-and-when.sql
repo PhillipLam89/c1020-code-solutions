456f122317422697635bf4edf548f3d72de7bf14
Phillip Lam <plam89@ucla.edu>
2020-12-03 14:44:12 -0800

    finished sql join

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,5 @@
+select "releaseYear",
+  "categories"."name" as "category"
+from "films"
+join "filmCategory" using ("filmId") join "categories" using ("categoryId")
+where "title" = 'Boogie Amelie';
