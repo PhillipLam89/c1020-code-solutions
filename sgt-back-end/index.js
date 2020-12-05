058cb8eddb3409a2b03c01dacb80b32acc6285f2
Phillip Lam <plam89@ucla.edu>
2020-12-05 15:29:30 -0800

    finished sgt back end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
-const { error } = require('console');
+
 const express = require('express');
 const app = express();
 const pg = require('pg');
+
+
 const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
@@ -17,9 +19,7 @@ app.get('/api/grades', (req, res) => {
   `;
   db.query(sql)
     .then(result => {
-      const grades = result.rows;
-      console.log(result.rows)
-      res.status(200).json(grades);
+      res.status(200).json(result.rows);
     })
     .catch(error => {
       res.status(500).json({ Error: 'An unexpected error has occurred.'})
@@ -52,13 +52,54 @@ app.post('/api/grades/', (req, res) => {
     });
 });
 
+
+app.put('/api/grades/:gradeId', (req, res) => {  //updates current id if it exists
+  const id = Number(req.params.gradeId)
+  const updatedName = req.body.name;
+  const updatedCourse = req.body.course;
+  const updatedScore = Number(req.body.score);
+
+  if (!Number.isInteger(id) || id < 1 || isNaN(id)) {
+    return res.status(400).json({error: `Invalid input, gradeId must be a positive integer only.`})
+  }
+  else if (!updatedCourse || !updatedName || !updatedScore) {
+    return res.status(404).json({ error: `The fields 'course', 'name', and 'score' are required` })
+  }
+  else if (updatedScore < 1 || updatedScore > 100) {
+    return res.status(404).json({ error: `scores must be between 1-100` })
+  }
+  const query = {
+    text: `
+      update "grades"
+        set "name" = $1,
+            "course" = $2,
+            "score" = $3
+      where "gradeId" = $4
+      returning *
+  `,
+    values : [updatedName, updatedCourse, updatedScore,id]
+  }
+  db.query(query)
+  .then(result => {
+    if(!result.rows[0]) {
+      res.status(404).json({ error: `Cannot find student with gradeId ${id} to update...` })
+    }
+    else if (result.rows[0]) {
+      res.status(201).json(result.rows[0])
+    }
+  })
+})
+
+
+
 app.delete('/api/grades/:gradeId', (req, res) => {
+
   const id = Number(req.params.gradeId)
   const updatedName = req.body.name;
   const updatedCourse = req.body.course;
   const updatedScore = Number(req.body.score);
   if (!Number.isInteger(id) || id < 1 || isNaN(id)) {
-    return res.status(400).json({ error: `GradeId must be a positive integar only...` });
+    return res.status(400).json({ error: `GradeId must be a positive integer only...` });
   }
   const query = {
     text: `
@@ -71,7 +112,7 @@ app.delete('/api/grades/:gradeId', (req, res) => {
   db.query(query)
     .then(result => {
       if (!result.rows[0]) {
-          res.status(404).json({error: `Cannot find student with gradeId ${id}`})
+          res.status(404).json({error: `Cannot find student with gradeId ${id} to delete...`})
       }
       else if (result.rows[0]) {
         res.status(201).send(`Success! student with gradeId ${id} has been deleted.`)
@@ -82,7 +123,10 @@ app.delete('/api/grades/:gradeId', (req, res) => {
       res.status(500).json({error: 'An expected error occured!'})
     });
 });
+
+
 app.listen(3000, () => {
   console.log('Express server listening on Port 3000');
+
 
 });
