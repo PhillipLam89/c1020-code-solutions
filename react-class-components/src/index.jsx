66c201b32a112561e886c3d726a1261187152823
Phillip Lam <plam89@ucla.edu>
2020-12-09 14:40:04 -0800

    fixed so that render has no arguments

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class CustomButton extends React.Component {
-  render(props) {
+  render() {
     return <button>{this.props.text}</button>;
   }
 }
