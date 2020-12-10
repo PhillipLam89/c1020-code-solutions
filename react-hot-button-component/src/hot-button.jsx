8592fc5a119f6a0b1425a91cb37910b4ef6c5338
Phillip Lam <plam89@ucla.edu>
2020-12-10 14:32:50 -0800

    removed unwanted variables

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 
-let counter = 0;
-let statusTracker = 0;
-const colors = ['purple', 'light-purple', 'red', 'orange', 'yellow', 'white'];
 
+const colors = ['purple', 'light-purple', 'red', 'orange', 'yellow', 'white'];
+let colorClass = null
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: null // default color
+      clicks: 0
     };
     this.handleClick = this.handleClick.bind(this); // binds this
   }
 
   handleClick() {
-    counter++;
-    if (counter === 3) {
-      counter = 0; // resets every 3 clicks as per requirements
-      statusTracker++; // every 3 click will change color
-      this.setState({
-        status: colors[statusTracker] // sets className which is linked to CSS colors
-      });
-    }
+    if (this.state.clicks > 18) this.state.clicks = 0
+    this.setState({
+      clicks: this.state.clicks +1
+    });
+    if (this.state.clicks === 3) colorClass = 'purple'
+    else if (this.state.clicks === 6) colorClass = 'light-purple'
+    else if (this.state.clicks === 9) colorClass = 'red'
+    else if (this.state.clicks === 12) colorClass = 'orange'
+    else if (this.state.clicks === 15) colorClass = 'yellow'
+    else if (this.state.clicks === 18) colorClass = 'white'
   }
 
   render() {
-    return <button className={this.state.status} onClick={this.handleClick}>I Miss Core HTML/JS</button>;
+    return <button class={colorClass} onClick={this.handleClick}>I Miss Core HTML/JS</button>;
   }
 }
 export default HotButton;
