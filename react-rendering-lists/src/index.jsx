3339d0818abb1a7a0da0cdcc3e97bd6fe3d584fe
Phillip Lam <plam89@ucla.edu>
2020-12-11 15:05:32 -0800

    fixed so that elements have keys

--- react-rendering-lists/src/index.jsx
+++ react-rendering-lists/src/index.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const pokedex = [
-  { number: '001', name: 'Bulbasaur' },
-  { number: '004', name: 'Charmander' },
-  { number: '007', name: 'Squirtle' },
-  { number: '025', name: 'Pikachu' },
-  { number: '039', name: 'Jigglypuff' }
-];
+class PokemonList extends React.Component {
+  render() {
+    const pokedex = [
+      { number: '001', name: 'Bulbasaur' },
+      { number: '004', name: 'Charmander' },
+      { number: '007', name: 'Squirtle' },
+      { number: '025', name: 'Pikachu' },
+      { number: '039', name: 'Jigglypuff' }
+    ];
 
-const pokedexList = pokedex.map((value) =>
-  <li>{value.name}</li>
-);
+    return (
+      <ul>
+        {pokedex.map(pokemon =>
+          <li key={pokemon.number}>
+            {pokemon.name}
+          </li>
+        )}
+      </ul>
+    );
+  }
+}
 
 ReactDOM.render(
-  <ul>{pokedexList}</ul>,
+  <PokemonList/>,
   document.getElementById('root')
 );
