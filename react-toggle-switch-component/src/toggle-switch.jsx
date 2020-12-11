8b786b04cd1588398996d4e929f7ac0254ee6d08
Phillip Lam <plam89@ucla.edu>
2020-12-10 17:20:57 -0800

    fixed to using elements instead of fontawesome

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -14,12 +14,25 @@ class ToggleSwitch extends React.Component {
   }
 
   render() {
+    let switchPosition = '';
+    let currentStatus = '';
     if (this.state.switchIsOn) {
-      return <i className="fas fa-toggle-on switchIsOn" onClick={this.handleClick}>ON!!</i>;
-    } else if (!this.state.switchIsOn) {
-      return <i className="fas fa-toggle-off switchIsOn" onClick={this.handleClick}>OFF!!</i>;
+      switchPosition = 'switch-is-on';
+      currentStatus = 'ON';
+    } else {
+      switchPosition = 'switch-is-off';
+      currentStatus = 'OFF';
     }
+
+    return (
+      <div className="container">
+        <div className={switchPosition} onClick={this.handleClick}>
+          <div className="back-part-of-switch"></div>
+          <div className="circular-icon"></div>
+        </div>
+        <p>{currentStatus}</p>
+      </div>
+    );
   }
 }
-
 export default ToggleSwitch;
